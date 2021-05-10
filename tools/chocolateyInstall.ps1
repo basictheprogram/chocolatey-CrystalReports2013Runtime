afefc0361f56e7adabf66fad0124b6bf6839dc02
Bob Tanner <tanner@real-time.com>
2021-05-10 15:16:01 -0500

    WIP: #1
      * Added private url to the installer

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -7,9 +7,9 @@ $params = @{
     packageName  = $package;
     fileType     = 'msi';
     silentArgs   = '/quiet';
-    url          = ''
+    url          = 'http://gold-images.int.celadonsystems.com/SYSPRO/CrystalRuntimes/CRRuntime_32bit_13_0_14.msi';
 
-    checksum     = '';
+    checksum     = '26196e58f9563cf58424530a72c58f6a33b6a15c06dff81027d5990cab7f3f28';
     checksumType = 'sha256';
 }
 
