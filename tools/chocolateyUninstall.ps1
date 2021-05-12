a6f188af7880da7c810d57d85b28fb1199669ae0
Bob Tanner <tanner@real-time.com>
2021-05-12 14:41:41 -0500

    WIP: #1 New package name requires changes in the uninstall

--- tools/chocolateyUninstall.ps1
+++ tools/chocolateyUninstall.ps1
@@ -1,9 +1,33 @@
-# HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\
-$msiId = '{80BDFEC8-E811-4211-A20E-98C569F84855}'
-$msiId64 = '{BBDB706C-1469-41D5-BAEF-FC43A996B5DE}'
+$ErrorActionPreference = 'Stop'; # stop on all errors
 
-$package = 'CrystalReports2010Runtime'
+$packageArgs = @{
+    packageName    = $env:ChocolateyPackageName
+    softwareName   = 'Crystal*Reports*runtime*'
+    fileType       = 'msi'
+    silentArgs     = "/qn /norestart"
+    validExitCodes = @(0, 3010, 1605, 1614, 1641)
+}
 
-if ([IntPtr]::Size -eq 8) { Uninstall-ChocolateyPackage $package 'MSI' -SilentArgs "$msiId64 /qb" -validExitCodes @(0) }
-else { Uninstall-ChocolateyPackage $package 'MSI' -SilentArgs "$msiId /qb" -validExitCodes @(0) }
+$uninstalled = $false
+[array]$key = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
 
+if ($key.Count -eq 1) {
+    $key | % {
+        $packageArgs['file'] = "$($_.UninstallString)"
+        if ($packageArgs['fileType'] -eq 'MSI') {
+            $packageArgs['silentArgs'] = "$($_.PSChildName) $($packageArgs['silentArgs'])"
+            $packageArgs['file'] = ''
+        }
+
+        Uninstall-ChocolateyPackage @packageArgs
+    }
+}
+elseif ($key.Count -eq 0) {
+    Write-Warning "$packageName has already been uninstalled by other means."
+}
+elseif ($key.Count -gt 1) {
+    Write-Warning "$($key.Count) matches found!"
+    Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+    Write-Warning "Please alert package maintainer the following keys were matched:"
+    $key | % { Write-Warning "- $($_.DisplayName)" }
+}
